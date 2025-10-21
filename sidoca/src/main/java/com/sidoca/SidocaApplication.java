6fe86fce6d438c28edf80273407f324a01881a65
Nick Wilsan <wilsannick55@gmail.com>
2025-10-21 20:55:17 +0700

    Add files via upload

---
+++ sidoca/src/main/java/com/sidoca/SidocaApplication.java
@@ -0,0 +1,13 @@
+package com.sidoca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SidocaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SidocaApplication.class, args);
+	}
+
+}
---
+++ sidoca/src/test/java/com/sidoca/SidocaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sidoca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SidocaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
