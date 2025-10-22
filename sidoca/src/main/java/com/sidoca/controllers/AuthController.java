b8610b88c6988539d818ca077164ae8fc8fbc0f8
Ihsan Ramadhan <ihsan.rmd@gmail.com>
2025-10-22 13:44:40 +0700

    AuthController
    AuthController, login, register, registerStyle

---
+++ sidoca/src/main/java/com/sidoca/controllers/AuthController.java
@@ -0,0 +1,18 @@
+package com.sidoca.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AuthController {
+
+    @GetMapping("/")
+    public String Login() {
+        return "login";
+    }
+
+    @GetMapping("/register")
+    public String Register() {
+        return "register";
+    }
+}
