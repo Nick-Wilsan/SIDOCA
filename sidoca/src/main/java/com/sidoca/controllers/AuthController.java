5bfd025260e0828a3e5f7783e2410b5409542424
Nick_Wilsan <wilsannick55@gmail.com>
2025-10-23 18:11:10 +0700

    Delete

--- sidoca/src/main/java/com/sidoca/controllers/AuthController.java
+++ sidoca/src/main/java/com/sidoca/controllers/AuthController.java
@@ -1,8 +1,5 @@
 package com.sidoca.controllers;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,10 +10,8 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.sidoca.Models.AkunModel;
 
-import org.springframework.web.bind.annotation.RequestParam;
 import jakarta.servlet.http.HttpSession;
 
-import com.sidoca.Models.ContohModel;
 import com.sidoca.Models.DataBaseClass.Akun;
 
 import ch.qos.logback.core.model.Model;
